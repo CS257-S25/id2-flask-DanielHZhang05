45b56dc89da8f8e2668cc525ccd68c4012fc3ba8
Daniel Zhang <danielzhang@Daniels-Laptop.local>
2025-04-23 15:08:46 -0500

    Completed flask app that shows the stats of one country

---
+++ ProductionCode/covid_stats.py
@@ -0,0 +1,55 @@
+import csv
+from datetime import datetime
+
+reader = csv.DictReader(open('ProductionCode/WHO-COVID-19-global-data.csv', 'r'))
+
+def load_data():
+    with open('ProductionCode/WHO-COVID-19-global-data.csv', 'r') as file:
+        reader = csv.DictReader(file)
+    return reader
+
+def stats(country, beginning_date, ending_date):
+    beginning = datetime.strptime(beginning_date, "%Y-%m-%d")
+    ending = datetime.strptime(ending_date, "%Y-%m-%d")
+    
+    total_cases = 0
+    total_deaths = 0
+
+    for row in reader:
+        if row['Country'] == country or row['Country_code'] == country:
+            date = datetime.strptime(row['Date_reported'], "%Y-%m-%d")
+            if beginning <= date <= ending:
+                if row['New_cases'] != '':
+                    total_cases += int(row['New_cases'])
+                if row['New_deaths'] != '':
+                    total_deaths += int(row['New_deaths'])
+    return total_cases, total_deaths
+
+
+def compare (countries, week):
+    for country in countries:
+        cases, deaths = stats(country, week, week)
+        print(f"Total cases in {country} during {week}: {cases}")
+        print(f"Total deaths in {country} from {week}: {deaths}\n")
+        
+    return cases, deaths
+
+
+'''
+def highest (week):
+    with open('Data/WHO-COVID-19-global-data.csv', 'r' ) as file:
+        reader = csv.DictReader(file)
+        highest_cases = 0
+        highest_country = ""
+        for row in reader:
+            if row['Date_reported'] == week:
+                if int(row['New_cases']) > highest_cases:
+                    highest_cases = int(row['New_cases'])
+                    highest_country = row['Country']
+        print(f"Country with the highest cases in {week}: {highest_country} with {highest_cases} cases")
+'''
+
+if __name__ == "__main__":
+    add_one()
+    print(reader)
+    
--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-'''Replace me with your flask app'''
+'''Flask app that has a basic route enabled'''
+from flask import Flask
+from ProductionCode import covid_stats
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def aboutpage():
+    return "Go to the url http://127.0.0.1:5000/covid_stats/Afghanistan/2021-01-01/2022-01-12 to get the total cases and deaths in a country between two dates"
+
+@app.route('/covid_stats/<country>/<beginning_date>/<ending_date>', strict_slashes=False)
+def covid_deaths(country, beginning_date, ending_date):
+    cases, deaths = covid_stats.stats(country, beginning_date, ending_date)
+    return f"Total cases in {country} is: {cases}, total deaths in {country} is: {deaths}"
+
+
+if __name__ == "__main__":
+    covid_stats.load_data()
+    app.run(debug=True)
