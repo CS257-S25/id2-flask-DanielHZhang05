9698632627083de5ec57d1d3e5760c2c47cf365d
Daniel Zhang <danielzhang@Daniels-Laptop.local>
2025-04-25 18:50:41 -0500

    second round of revisions

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -6,12 +6,12 @@ import app
 class TestCovidStats(unittest.TestCase):
     """This class tests our flask app"""
     def test_main(self):
-       """Test the homepage function of the app."""
-       self.assertEqual(app.aboutpage, """To access Go to the url
+        """Test the homepage function of the app."""
+        self.assertEqual(app.aboutpage, """To access Go to the url
        http://127.0.0.1:5000/covid_stats/Afghanistan/2021-01-01/2022-01-12 
        to get the total cases and deaths in Afghanistan between 2021-01-01 to 2022-01-12""")
 
     def test_aboutpage(self):
-       """Test the about page of the app."""
-       self.assertEqual(app.covid_deaths('Afghanistan', '2021-01-01', '2021-01-12'),
+        """Test the about page of the app."""
+        self.assertEqual(app.covid_deaths('Afghanistan', '2021-01-01', '2021-01-12'),
        "Total cases in Afghanistan is: 1641, total deaths in Afghanistan is: 119")
