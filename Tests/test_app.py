54e2c385f38c8d5c0ce51fcbb5eb40b972bfdc6f
Daniel Zhang <danielzhang@Daniels-Laptop.local>
2025-04-25 18:30:33 -0500

    Finished id2

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -1,1 +1,19 @@
+"""Tests for app.py"""
+import unittest
+import sys
+import app
+from io import StringIO
 
+class TestCovidStats(unittest.TestCase):
+   """This class tests our flask app"""
+   def test_main(self):
+      """Test the main function of the app."""
+      sys.argv = ['cl.py', 'stats', 'Afghanistan', '2020-01-01', '2020-01-12']
+      sys.stdout = StringIO()
+      app.main()
+      printed_output = sys.stdout.getvalue()
+      self.assertEqual(printed_output, "To access Go to the url http://127.0.0.1:5000/covid_stats/Afghanistan/2021-01-01/2022-01-12 to get the total cases and deaths in Afghanistan between 2021-01-01 to 2022-01-12")
+
+   def test_aboutpage(self):
+      """Test the about page of the app."""
+      self.assertEqual(app.covid_deaths('Afghanistan', '2021-01-01', '2021-01-12'), "Total cases in Afghanistan is: 0, total deaths in Afghanistan is: 0")
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''Flask app that has a basic route enabled'''
+'''Flask app that has a basic route enabled for our covid stats function'''
 from flask import Flask
 from ProductionCode import covid_stats
 
@@ -7,14 +7,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def aboutpage():
-    return "Go to the url http://127.0.0.1:5000/covid_stats/Afghanistan/2021-01-01/2022-01-12 to get the total cases and deaths in a country between two dates"
+    """This is the home page that tells the user how to input the data."""
+    return "To access Go to the url http://127.0.0.1:5000/covid_stats/Afghanistan/2021-01-01/2022-01-12 to get the total cases and deaths in Afghanistan between 2021-01-01 to 2022-01-12"
 
 @app.route('/covid_stats/<country>/<beginning_date>/<ending_date>', strict_slashes=False)
 def covid_deaths(country, beginning_date, ending_date):
+    """Function to get the total cases and deaths in a country between two dates."""
     cases, deaths = covid_stats.stats(country, beginning_date, ending_date)
     return f"Total cases in {country} is: {cases}, total deaths in {country} is: {deaths}"
 
-
-if __name__ == "__main__":
+def main():
+    """Main function to run the Flask app."""
     covid_stats.load_data()
     app.run(debug=True)
+
+if __name__ == "__main__":
+    main()
